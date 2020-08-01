b43835151de25424ebb36ddddc3659aaada3309d
Shriya <shriya2017das@gmail.com>
2020-08-01 17:36:13 +0530

    User BMI and Calorie

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
@@ -92,7 +91,6 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        
     }
 }
 
