efc4138dcf7a6876a166f978af6670a6e201e672
niteshpr <niteshpr.99@gmail.com>
2020-08-01 19:39:41 +0530

    Front-end and views

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
 
 
 REDIS_HOST = 'localhost'
@@ -91,6 +93,7 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        
     }
 }
 
