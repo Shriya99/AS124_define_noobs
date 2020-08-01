265474edc1089d297417154fd6cec7e435114514
niteshpr <niteshpr.99@gmail.com>
2020-08-02 00:24:06 +0530

    frontend update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,9 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
-
 
 DEBUG = True
 
@@ -94,7 +92,7 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-       
+        
     }
 }
 
