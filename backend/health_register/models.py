e18f26a52f541511208c2a45e8495326980b1468
niteshpr <niteshpr.99@gmail.com>
2020-08-03 08:20:11 +0530

    email filter in registrartion -non  repeting, heathcard updated, other front end changes

--- backend/health_register/models.py
+++ backend/health_register/models.py
@@ -4,3 +4,4 @@ from django.db import models
 # Create your models here.
 class hmail(models.Model):
     email = models.EmailField(max_length=20)
+    account = models.BooleanField(default = False)
--- backend/health_register/views.py
+++ backend/health_register/views.py
@@ -1,34 +1,40 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import NewUserForm
+from django.contrib.auth import login,logout,authenticate
 from .models import hmail
+from django.shortcuts import redirect
 
 def newstaff(request):
     if request.method == "POST":
-        username=request.POST.get('uname')
-        em=request.POST.get('id_email')
-        pass1=request.POST.get('password1')
-        pass2=request.POST.get('password2')
-        if pass1==pass2:
-            mail=hmail.objects.filter(email=em)
-            if mail:
-                print('yes')
-                newuser = User() 
-                username = form.cleaned_data.get('username')
-                login(request, user)
-                return redirect("main:home")
-
+        form =NewUserForm(request.POST)
+        em=request.POST.get('email')
+        print(em)
+        mail=hmail.objects.filter(email=em)
+        if mail:
+            mail1=hmail.objects.get(email=em)
+            if mail1.account:
+                return render(request,"failure.html" ,{'message':'Account Already Exist' ,'data':"Try Again", 'link':'/newreg/new_staff/'})
             else:
-                for msg in form.error_messages:
-                    print(form.error_messages[msg])
+                if form.is_valid():
+                    mail1.account=True
+                    mail1.save()
+                    user = form.save()
+                    username = form.cleaned_data.get('username')
+                    login(request, user)
+                    return redirect("main:home")
 
-                return render(request = request,
-                            template_name = "userreg.html",
-                            context={"form":form})
+                else:
+                    for msg in form.error_messages:
+                        print(form.error_messages[msg])
+
+                    return render(request = request,
+                                template_name = "userreg.html",
+                                context={"form":form})
         else:
             print('NO')
             return render(request,"failure.html" ,{'message':'Not-authorised Health User' ,'data':"Try Again", 'link':'/newreg/new_staff/'})
-            
     else:
         form = NewUserForm
         return render(request = request,
