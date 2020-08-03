deef3647a6cfc33efda4a1c6721b37bddc9e5f01
niteshpr <niteshpr.99@gmail.com>
2020-08-03 19:35:24 +0530

    done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,9 +27,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
 
 
 REDIS_HOST = 'localhost'
@@ -97,6 +94,7 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        
 
     }
 }
